406fb8c136e356b8f88386591852302adb60b5a5
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2023-11-29 10:33:31 +0200

    clear project

--- LoganCoghill-Portfolio/src/App.jsx
+++ LoganCoghill-Portfolio/src/App.jsx
@@ -1,19 +1,5 @@
-import Header from "./Components/Header";
-
-import { useState } from "react";
-import HomePage from "./Components/HomePage";
-import Footer from "./Components/Footer";
-
 function App() {
-  const [page, setPage] = useState("home");
-
-  return (
-    <>
-      <Header setPage={setPage} />
-      {page === "home" && <HomePage />}
-      <Footer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- LoganCoghill-Portfolio/src/Components/Header.jsx
+++ LoganCoghill-Portfolio/src/Components/Header.jsx
@@ -1,31 +1,3 @@
-export default function Header({ setPage }) {
-  return (
-    <header className="bg-stone-700">
-      <menu>
-        <ul className="flex flex-row gap-8 justify-end px-10 py-7">
-          <li>
-            <button onClick={() => setPage("home")} className="hover:underline">
-              <strong>Home</strong>
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => setPage("about")}
-              className="hover:underline"
-            >
-              <strong>About</strong>
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => setPage("contact")}
-              className="hover:underline"
-            >
-              <strong>Contact</strong>
-            </button>
-          </li>
-        </ul>
-      </menu>
-    </header>
-  );
+export default function Header(){
+    
 }
